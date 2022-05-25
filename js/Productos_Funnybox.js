24ba1ba5f2e2acd5eff315e57b889f398924fcb5
Jigeroar87 <jimmy.robles.a@gmail.com>
2022-05-25 14:56:12 -0500

    Desafio Entregable 2-avance

---
+++ js/Productos_Funnybox.js
@@ -0,0 +1,41 @@
+
+/*
+ * Clase que modela la entidad Productos_Funnybox
+ */
+class Productos_Funnybox {
+
+    /*
+     * Constructor
+     * @param {*} modelo del auto
+     * @param {*} marca del auto
+     * @param {*} precio  del auto
+     */
+    constructor (nombre,precio,oferta){ 
+
+
+        this.nombre = nombre;  /*modelo*/
+        this.precio = precio ; /*marca*/
+        this.oferta = oferta ; /*precio*/
+        this.id = -1 ;
+
+    }
+
+
+    /*
+     * Muestra la descripcion completa del auto     
+     */
+
+    mostrar_descripcion(){
+        return (this.id + " - " +this.nombre + " - " + this.precio + " - $" + this.oferta) ;
+    }
+
+    /*
+     * Setea un nuevo id
+     * @param {*} nuevo_id a setear
+     */
+    set_id(nuevo_id){
+        this.id = nuevo_id;
+    }
+
+    
+}
