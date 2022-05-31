4e4a42994e7c1aefa056eda5fc22ca205e25bafa
Jigeroar87 <jimmy.robles.a@gmail.com>
2022-05-31 12:37:52 -0500

    Desafio Entregable 3 Dom y Eventos

---
+++ js/Dom_Eventos.js
@@ -0,0 +1,31 @@
+
+//MOUSEOVER Y MOUSEOUT
+let items = document.getElementsByClassName('nav-link text-dark');
+for(let i = 0; i < items.length; i++){
+    //MOUSEOVER
+    items[i].addEventListener('mouseover', () => {
+        items[i].style.backgroundColor = "#D7BDE2";
+        items[i].classList.add("text-white"); 
+    })    
+    //MOUSEOUT
+    items[i].addEventListener('mouseout', () => {
+        items[i].style.backgroundColor = "";
+        items[i].classList.remove("text-white"); 
+    })
+}
+
+//FOCUS y BLUR
+let inputFocus = document.getElementById("MensajeLargo");
+let msjRecomendation = document.getElementById("msjRecomendation");
+//FOCUS
+inputFocus.addEventListener('focus', ()=>{
+    
+        inputFocus.style.border = "2px solid #5a5ada";
+        msjRecomendation.textContent = "*Se recomienda que el texto tenga como máximo 250 caracteres";
+    
+})
+//BLUR
+inputFocus.addEventListener('blur', ()=>{
+    inputFocus.style.border = "1px solid black";
+    msjRecomendation.textContent = "";
+})
