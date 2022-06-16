81c7088045c930570a11e8be91116e372c9a1931
Jigeroar87 <jimmy.robles.a@gmail.com>
2022-06-15 19:48:28 -0500

    Avance trabajo con filtros

--- js/Carga_Productos.js
+++ js/Carga_Productos.js
@@ -4,70 +4,90 @@ let items = [
         "Precio": "Precio Regular: S/ 15",
         "Oferta": "Oferta: S/ 10",
         "Foto": "../imagenes/Productos/Alcancia_Minie.JPG",
-        "Tipo": "Alcancia"
+        "Tipo": "Alcancia",
+        "ID": 1,
+        "Soles": 10
     },
     {
         "Producto" : "Alcancia Unicornio",
         "Precio": "Precio Regular: S/ 15",
         "Oferta": "Oferta: S/ 10",
         "Foto": "../imagenes/Productos/Alcancia_Unicornio.JPG",
-        "Tipo": "Alcancia"
+        "Tipo": "Alcancia",
+        "ID": 2,
+        "Soles": 10
     },
     {
         "Producto" : "Alcancia Pequeña Lulú",
         "Precio": "Precio Regular: S/ 15",
         "Oferta": "Oferta: S/ 10",
         "Foto": "../imagenes/Productos/Alcancia_Lulu.JPG",
-        "Tipo": "Alcancia"
+        "Tipo": "Alcancia",
+        "ID": 3,
+        "Soles": 10
     },
     {
         "Producto" : "Alcancia Increíble Hulk",
         "Precio": "Precio Regular: S/ 15",
         "Oferta": "Oferta: S/ 10",
         "Foto": "../imagenes/Productos/Alcancia_Hulk.JPG",
-        "Tipo": "Alcancia"
+        "Tipo": "Alcancia",
+        "ID": 4,
+        "Soles": 10
     },
     {
         "Producto" : "Alcancia Dark Vader",
         "Precio": "Precio Regular: S/ 15",
         "Oferta": "Oferta: S/ 10",
         "Foto": "../imagenes/Productos/Alcancia_Darkvader.JPG",
-        "Tipo": "Alcancia"
+        "Tipo": "Alcancia",
+        "ID": 5,
+        "Soles": 10
     },
     {
         "Producto" : "Lienzo Personalizado Pikachu",
         "Precio": "Precio Regular: S/ 10",
         "Oferta": "Oferta: S/ 8",
         "Foto": "../imagenes/Productos/Lienzo_Pikachu.JPG",
-        "Tipo": "Lienzo"
+        "Tipo": "Lienzo",
+        "ID": 6,
+        "Soles": 8
     },
     {
         "Producto" : "Lienzo Personalizado Woody",
         "Precio": "Precio Regular: S/ 10",
         "Oferta": "Oferta: S/ 8",
         "Foto": "../imagenes/Productos/Lienzo_Woody.JPG",
-        "Tipo": "Lienzo"
+        "Tipo": "Lienzo",
+        "ID": 7,
+        "Soles": 8
     },
     {
         "Producto" : "Lienzo Personalizado Nopo y Gonta",
         "Precio": "Precio Regular: S/ 10",
         "Oferta": "Oferta: S/ 8",
         "Foto": "../imagenes/Productos/Lienzo_Nopoygonta.JPG",
-        "Tipo": "Lienzo"
+        "Tipo": "Lienzo",
+        "ID": 8,
+        "Soles": 8
     },
     {
         "Producto" : "Lienzo Personalizado Payasito",
         "Precio": "Precio Regular: S/ 10",
         "Oferta": "Oferta: S/ 8",
         "Foto": "../imagenes/Productos/Lienzo_Payasito.JPG",
-        "Tipo": "Lienzo"
+        "Tipo": "Lienzo",
+        "ID": 9,
+        "Soles": 8
     },
     {
         "Producto" : "Lienzo Personalizado Llama",
         "Precio": "Precio Regular: S/ 10",
         "Oferta": "Oferta: S/ 8",
         "Foto": "../imagenes/Productos/Lienzo_Llama.JPG",
-        "Tipo": "Lienzo"
+        "Tipo": "Lienzo",
+        "ID": 10,
+        "Soles": 8
     },
 ]
 
@@ -91,8 +111,13 @@ function createitemCard(item) {
   let producto = document.createElement('p');
   producto.innerHTML = `<p>${item.Producto} <br> ${item.Precio} <br> ${item.Oferta} </p>`;
 
+  let boton = document.createElement('button'); 
+  boton.className = "btn btn-outline-dark btn-lg agregar-carrito";
+  boton.innerHTML = `<a data-id=${item.ID}>Comprar</a>`;
+
   cardBody.appendChild(imagen);
   cardBody.appendChild(producto);
+  cardBody.appendChild(boton);
   card.appendChild(cardBody);
   cardContainer.appendChild(card);
 
